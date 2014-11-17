e49e487c48b5d80adc403d469bcf3dca3a7d9755
kugg <kugghjul@gmail.com>
2014-11-17 23:58:52 +0100

    Updated wifirssi.py license
    Updated license and clarified dependency origin of matplotlib.

--- wifirssi.py
+++ wifirssi.py
@@ -1,15 +1,30 @@
 """
-Sample program to detect signal failures in Wifi.
-GNU/GPLv3 Christoffer Jerkeby 2014
+wifirssi - an application to detect strength of Wifi signals.
+
+Copyright (c) 2014 Christoffer Jerkeby <kugghjul@gmail.com>
+wifirssi is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+ 
+wifirssi is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along
+with wifirssi; see the file COPYING.  If not, write to the Free Software
+Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
 Dependencies:
 
  * pythonwifi
- * matplotlib (python builtin)
+ * matplotlib
  * linux
 
 Installation:
 
-$ pip install pythonwifi
+$ pip install pythonwifi matplotlib
 
 Usage:
 
